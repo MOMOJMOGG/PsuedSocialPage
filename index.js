b2a21f0ad2d7809ab51ddec5a79c0e19732bf5cb
MOMOJMOGG <momomacaca@gmail.com>
2021-03-14 21:20:11 +0800

    Finish version 1

--- index.js
+++ index.js
@@ -4,6 +4,36 @@ const INDEX_URL = BASE_URL + '/api/v1/users/'
 const friends = []
 const dataPanel = document.querySelector('#data-panel')
 
+// Modal 顯示
+function showUserModal(id) {
+  function matchIdFromList(user) {
+    return user.id === id
+  }
+
+  const userAvatar = document.querySelector('#user-avatar')
+  const userInfo = document.querySelector('#user-info')
+
+  const targetUser = friends.find(matchIdFromList)
+
+  userAvatar.src = targetUser.avatar
+  const contentHTML = `
+      <li>Email: ${targetUser.email}</li>
+      <li>Gender: ${targetUser.gender}</li>
+      <li>Age: ${targetUser.age}</li>
+      <li>Region: ${targetUser.region}</li>
+      <li>Birthday: ${targetUser.birthday}</li>
+  `
+
+  userInfo.innerHTML = contentHTML
+}
+
+// 監聽 data panel
+dataPanel.addEventListener('click', function onPanelClicked(event) {
+  if (event.target.tagName === "IMG") {
+    showUserModal(Number(event.target.dataset.id))
+  }
+})
+
 // 放資料進網頁
 function renderFirendList(data) {
   let contentHTML = ``
@@ -13,8 +43,8 @@ function renderFirendList(data) {
     contentHTML += `
         <div class="col-2">
           <div class="m-1">
-            <div class="card" data-toggle="modal" data-target="#user-modal" data-id="${item.id}">
-              <img src="${item.avatar}" class="card-img-top" alt="Fake User" />
+            <div class="card">
+              <img src="${item.avatar}" class="card-img-top" alt="Fake User" data-toggle="modal" data-target="#user-modal" data-id="${item.id}"/>
               <div class="card-body">
                 <h5 class="card-title text-center">${item.name + item.surname}</h5>
               </div>
@@ -33,5 +63,6 @@ axios
   .then((response) => {
     friends.push(...response.data.results)
     renderFirendList(friends)
+    console.log(friends)
   })
   .catch((err) => console.log(err))
