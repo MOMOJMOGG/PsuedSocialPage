35761da77e5a6a5d7acefeffaeab4591aee08854
MOMOJMOGG <momomacaca@gmail.com>
2021-03-21 00:38:09 +0800

    Add removeFromFavorite

--- SocialList-v2/index.js
+++ SocialList-v2/index.js
@@ -8,7 +8,7 @@ let displayFriend = NaN
 const dataPanel = document.querySelector('#data-panel')
 const searchInput = document.getElementById('nav-searching-input')
 const searchBtn = document.getElementById('nav-searching-btn')
-const modalBtnAddFavorite = document.getElementById('modal-byn-add-favorite')
+const modal = document.querySelector('.modal')
 // ***************************************************************************** Function
 /*** Modal 顯示 ***/
 function showUserModal(id) {
@@ -33,6 +33,17 @@ function showUserModal(id) {
   `
 
   userInfo.innerHTML = contentHTML
+
+  // 判斷是否存在於 收藏清單中
+  const list = JSON.parse(localStorage.getItem('closeFriends')) || []
+
+  if (list.some(matchIdFromList)) {
+    $('#modal-btn-remove-favorite').show()
+    $('#modal-btn-add-favorite').hide()
+  } else {
+    $('#modal-btn-remove-favorite').hide()
+    $('#modal-btn-add-favorite').show()
+  }
 }
 
 $('.modal').on('hidden.bs.modal', (event) => {
@@ -62,6 +73,50 @@ function renderFirendList(data) {
   dataPanel.innerHTML = contentHTML
 }
 
+/*** 加入 收藏清單 ***/
+function addToFavoriteList() {
+  if (displayFriend === NaN) {
+    return
+  }
+
+  function matchIdFromList(user) {
+    return user.id === displayFriend
+  }
+
+  const targetUser = friends.find(matchIdFromList)
+
+  // 從 local Storage 取得 收藏清單 資料
+  const list = JSON.parse(localStorage.getItem('closeFriends')) || []
+
+  // 錯誤處理 : 重複收藏
+  if (list.some(matchIdFromList)) {
+    return alert('此用戶已在收藏清單中！')
+  }
+
+  // 加入 收藏清單 並 更新 local Storage
+  list.push(targetUser)
+  localStorage.setItem('closeFriends', JSON.stringify(list))
+}
+
+/*** 刪除 收藏清單 ***/
+function RemoveFromFavoriteList() {
+  if (displayFriend === NaN) {
+    return
+  }
+
+  function matchIdFromList(user) {
+    return user.id === displayFriend
+  }
+
+  // 從 收藏清單 移除用戶
+  const list = JSON.parse(localStorage.getItem('closeFriends')) || []
+
+  const friendIndex = list.findIndex(matchIdFromList)
+
+  list.splice(friendIndex, 1)
+
+  localStorage.setItem('closeFriends', JSON.stringify(list))
+}
 // ***************************************************************************** Event Listener
 /*** 監聽 data panel ***/
 dataPanel.addEventListener('click', function onPanelClicked(event) {
@@ -94,29 +149,18 @@ searchBtn.addEventListener('click', function onSearchBtnClicked(event) {
   renderFirendList(filteredFriends)
 })
 
-/*** 監聽 Modal 收藏按鈕 ***/
-modalBtnAddFavorite.addEventListener('click', function onModalAddFavoriteClicked(event) {
-  if (displayFriend === NaN) {
-    return
+/*** 監聽 Modal 按鈕 ***/
+modal.addEventListener('click', (event) => {
+  const target = event.target
+  if (target.id === 'modal-btn-add-favorite') {
+    addToFavoriteList()
+    $('#modal-btn-remove-favorite').toggle()
+    $('#modal-btn-add-favorite').toggle()
+  } else if (target.id === 'modal-btn-remove-favorite') {
+    RemoveFromFavoriteList()
+    $('#modal-btn-add-favorite').toggle()
+    $('#modal-btn-remove-favorite').toggle()
   }
-
-  function matchIdFromList(user) {
-    return user.id === displayFriend
-  }
-
-  const targetUser = friends.find(matchIdFromList)
-
-  // 從 local Storage 取得 收藏清單 資料
-  const list = JSON.parse(localStorage.getItem('closeFriends')) || []
-
-  // 錯誤處理 : 重複收藏
-  if (list.some(matchIdFromList)) {
-    return alert('此用戶已在收藏清單中！')
-  }
-
-  // 加入 收藏清單 並 更新 local Storage
-  list.push(targetUser)
-  localStorage.setItem('closeFriends', JSON.stringify(list))
 })
 
 // 請求資料
